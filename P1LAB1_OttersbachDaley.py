219fb642ffe47cfa4490135434ed4bdb19a78873
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2024-11-12 11:25:20 -0500

    First submission

---
+++ P1LAB1_OttersbachDaley.py
@@ -0,0 +1,11 @@
+# Daley Ottersbach
+# 11/12/2024
+# P1LAB1
+# Welcomes a user after they input their name
+
+
+# The static typing is just habit to catch errors when working on larger projects.
+firstName: str = input("Enter first name: ")
+lastName: str = input("Enter last name: ")
+
+print(f"Welcome to CTI-110 {firstName} {lastName}.")
